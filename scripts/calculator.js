078ac3a44cd26925555920ba30795797eb34fcbb
ThurFerreira <arthurfsilva110@gmail.com>
2023-01-18 23:15:18 -0300

    v1

---
+++ scripts/calculator.js
@@ -0,0 +1,2 @@
+//declarando uma nova variavel dentro de "window", deixando assim o objeto "calculadora" global, podendo ser acessado através do console do browser
+window.calculadora = new CalcController();
